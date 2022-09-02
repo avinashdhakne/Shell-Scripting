141af55edd411b285206faa702a80b4b62ba2541
Avinash <asdhakne21@gmail.com>
2022-09-02 20:17:59 +0530

    Update code

--- 2 Variables.sh
+++ 2 Variables.sh
@@ -1,33 +1,36 @@
-#! /usr/bin/bash
-
 # Variables are of two types 
+
 # 1. System Variables:
 # - Created and maintained by system
 # - They are in capital letters
-
-# 2. User defined Variables:
-# - Created and maintained by the User
-# - No naming convention
 
 echo System Variables
 echo $BASH
 echo $BASH_VERSION
 echo $HOME
 echo $PWD
-
 echo
+
+# 2. User defined Variables:
+# - Created and maintained by the User
+# - No naming convention
+
 echo User defined Variables
 name=Avinash
 echo This is name: $name
 echo "This is name: "$name
 
-echo Variables should not be start with number
+# Variables should not be start with number
 22var=Dhakne
 echo &22var # Error
 
-echo Variables are case sensitive
+# Variables are case sensitive
 VAR=name
 echo $var # Error
 
+input string with spaces
 var1="This is String variable"
 echo $var1
+
+var2=This is String variable
+echo $var2 # Error
--- 3 User_input.sh
+++ 3 User_input.sh
@@ -1,1 +1,45 @@
-echo hello
+User Input
+
+Ente will terminate the user input
+
+# Single Input
+echo Enter the variable 
+read name
+echo "Entered Variable is $name"
+
+# For two user input press SPACE for next input
+echo
+# Multiple inputs
+echo "Enter Your name and surname: "
+read initial surname
+echo Your name: $initial 
+echo Your Surname: $surname 
+
+# Enter your input at same line using -p flag
+read -p "User name: " userName
+echo Your user name is: $userName
+
+# Enter password using -s flag
+echo Enter your password: 
+read -s password
+echo Your password is: $password
+
+# Enter User infomation
+read -p "User Name: " userName1
+read -sp "Password: " password1
+
+echo 
+echo
+echo User infomation is
+echo userName: $userName1
+echo password: $password1
+
+# Store the input into array using -a flag
+echo "Enter Your Friends: "
+read -a friend
+echo "Your Friends are: ${friend[0]}, ${friend[1]}, ${friend[2]}"
+
+# If you havent store the input in any variable then it get stored in default variable 'REPLY'
+echo "Enter the variable: "
+read 
+echo "Your variable is: $REPLY"
