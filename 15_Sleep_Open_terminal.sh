faa4285cfcb2bf748b62baf294027a38948a10dd
Avinash <asdhakne21@gmail.com>
2022-09-03 23:52:46 +0530

    sleep and open terminals

---
+++ 15_Sleep_Open_terminal.sh
@@ -0,0 +1,20 @@
+num1=1
+# Sleep function
+while [ $num1 -le 3 ]
+do 
+    echo $num1
+    (( num1++ ))
+    # this will give the delay of 1 sec for every iteration
+    sleep 1
+done
+
+num2=1
+# Open terminal
+while [ $num2 -le 3 ]
+do 
+    echo $num2
+    (( num2++ ))
+    # This will open the one terminal for every iteration
+    # syntex -> terminal_name &
+    cons0 &
+done
