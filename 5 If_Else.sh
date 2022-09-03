4ed54153d0d7d6d9cafa905a10b045193569ab72
Avinash <asdhakne21@gmail.com>
2022-09-03 11:56:51 +0530

    if else

--- 5 If_Else.sh
+++ 5 If_Else.sh
@@ -19,3 +19,45 @@
 #   <   - is less than, in ASCII alphabetical order     - if [[ "$a" < "$b" ]]
 #   >   - is greater than, in ASCII alphabetical order  - if [[ "$a" > "$b" ]]
 #   -z  - string is null, that is, has zero length
+
+# If-Else condition
+num=10
+if [ $num -eq 10 ]
+# Remainber dont forgot to give a space before and after the condition
+then 
+    echo Number is 10
+fi
+
+# For arithmatic operators we could use double bracket also 
+if (($num > 7))
+then 
+    echo Number is 10
+fi
+
+# String comparision
+word=avinash
+if [ $word = avinash ]
+then 
+    echo Name is same
+fi
+
+char=a
+# in case of <, > and -z  operators use [[ ]] brakets
+if [[ $char < b ]]
+then 
+    echo b is larger than a
+fi
+
+# if-else ladder
+marks=15
+
+if (( $marks >= 80 ))
+then 
+    echo Congratulations!!
+elif (( $marks >= 35 ))
+then 
+    echo Your are pass
+else
+    echo You failed!
+fi
+
