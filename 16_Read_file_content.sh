d5aa9e31068aceea3c19b7242b2afcc87bfbf9be
Avinash <asdhakne21@gmail.com>
2022-09-11 09:33:47 +0530

    read file

---
+++ 16_Read_file_content.sh
@@ -0,0 +1,21 @@
+# Approach 1: 
+while read line
+do 
+    echo $line
+    sleep 3
+done < test.txt
+# '<' this input redirection symbol direct the stream of input characters from file to the while loop
+
+# Approach 2: 
+cat test.txt | while read line
+do 
+    echo $line
+    sleep 3
+done
+
+# Approach 3: IFS (Internal field seperator)
+while IFS=' ' read line
+do 
+    echo $line
+    sleep 3
+done < test.txt
