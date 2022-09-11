96925e748dd73dd4f3194833d07048d8b7252016
Avinash <asdhakne21@gmail.com>
2022-09-11 10:44:40 +0530

    select loop

---
+++ 19_Select_Loop.sh
@@ -0,0 +1,31 @@
+# syntex:
+    # select var in list
+    # do 
+    #     command1
+    #     command2
+    #     commandN
+    # done
+
+# - SELECT COMMAND Constructs simple menu from word list. 
+# - It Allows user to enter a number instead of a word. 
+# - So User enters sequence number corresponding to the word.
+
+select country in India USA China Russia Japan
+do 
+    echo "$country is Selected :)"
+done 
+
+# we could use select loop with the swith case statement
+select country in India USA China Russia Japan
+do 
+    echo Country : $country
+    echo Capital : 
+    case $country in 
+    India   ) echo New Delhi ;;
+    USA     ) echo Washington DC ;;
+    China   ) echo Bejing ;;
+    Russia  ) echo Moscow ;;
+    Japan   ) echo Tokyo ;;
+    *       ) echo Invalid Input ;;
+    esac
+done
